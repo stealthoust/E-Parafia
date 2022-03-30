0d6c2c7f1726448176c4233af3d23f36d792284f
stealthoust <stealthoust@gmail.com>
2022-03-30 20:19:29 +0200

    Dodanie metod zmieniajacych formularze

--- client/src/main/java/edu/pwste/PrimaryController.java
+++ client/src/main/java/edu/pwste/PrimaryController.java
@@ -6,8 +6,32 @@ import javafx.fxml.FXML;
 
 public class PrimaryController {
 
+
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
     }
+    @FXML
+    private void switchToFormularz() throws IOException {
+        App.setRoot("formularz");
+    }
+    @FXML
+    private void switchToKalendarz() throws IOException {
+        App.setRoot("kalendarz");
+    }
+    @FXML
+    private void switchToKsiegaOsob() throws IOException {
+        App.setRoot("ksiega_osob");
+    }
+    @FXML
+    private void switchToMszaDodaj() throws IOException {
+        App.setRoot("msza_dodaj");
+    }
+    @FXML
+    private void switchToMszaInfo() throws IOException {
+        App.setRoot("msza_info");
+    }
+
 }
+
+
