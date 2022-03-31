15e8b3b571a85f6e34ffb9091d3eaea2cbac2210
marceq1337 <mbotwina2+git@gmail.com>
2022-03-31 10:18:43 +0200

    first build

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mysql = require("mysql");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const conn = mysql.createConnection({
+  host: "studweb.edu.int.pwste.edu.pl",
+  user: "stealthoust",
+  password: "INFormatyki!@",
+  database: "parafia",
+});
+
+
+app.get('/osoby', (req, res) => {
+  conn.query('SELECT * FROM osoby',(err,rows,fields)=>{
+    if(!err)
+      res.json(rows);
+    else
+      console.log(err);
+
+  })
 })
 
 app.listen(port, () => {
