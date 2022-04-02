dabf05afc5be014b6aeb5bc052cecb8e56429364
stealthoust <stealthoust@gmail.com>
2022-04-02 12:26:57 +0200

    obiektowe metody konfiguracji table view oraz jej wypełnienia, gettery i settery do klasy Osoba

--- client/src/main/java/edu/pwste/KsiegaOsob.java
+++ client/src/main/java/edu/pwste/KsiegaOsob.java
@@ -11,22 +11,36 @@ import javafx.scene.control.TableView;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import com.google.gson.Gson;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class KsiegaOsob implements Initializable {
 
     public TableView table_view=new TableView();
-    TableColumn col_id=new TableColumn("Id");
-    TableColumn col_imie =new TableColumn("Imie");
-    TableColumn col_nazwisko=new TableColumn("Nazwisko");
-    TableColumn col_data=new TableColumn("Nazwisko");
-    TableColumn col_miejscowosc=new TableColumn("Nazwisko");
-    public String url = "http://10.8.42.15:3000/osoby";
+    @FXML
+    TableColumn <Osoba,Integer> col_id;
+    @FXML
+    TableColumn <Osoba,String> col_imie ;
+    @FXML
+    TableColumn <Osoba,String> col_nazwisko;
+    @FXML
+    TableColumn <Osoba, Date> col_data;
+    @FXML
+    TableColumn <Osoba,String> col_miejscowosc;
+    @FXML
+    TableColumn <Osoba,Integer> col_id_ksiedza;
 
-    public Osoba[] pobierzDane() throws Exception
+
+
+    public String url = "http://localhost:3000/osoby";
+
+
+        public ObservableList<Osoba> pobierzDane() throws Exception
     {
 
         Gson gson = new Gson();
@@ -36,17 +50,30 @@ public class KsiegaOsob implements Initializable {
         var response = client.send(request, HttpResponse.BodyHandlers.ofString());
         String odpowiedz = response.body();
         Osoba[] enums = gson.fromJson(odpowiedz,Osoba[].class);
+        ObservableList<Osoba> lista = FXCollections.observableArrayList(enums);
 
-
-        return enums;
+        return lista;
 
 
     }
 
-    public void uzupelnijListe() throws Exception {
-        table_view.getColumns().addAll(col_id,col_imie,col_nazwisko,col_data,col_miejscowosc);
+    private void tableViewSetup()
+    {
+        col_id.setCellValueFactory(new PropertyValueFactory<>("id"));
+        col_imie.setCellValueFactory(new PropertyValueFactory<>("imie"));
+        col_nazwisko.setCellValueFactory(new PropertyValueFactory<>("nazwisko"));
+        col_data.setCellValueFactory(new PropertyValueFactory<>("data_urodzenia"));
+        col_miejscowosc.setCellValueFactory(new PropertyValueFactory<>("miejscowosc"));
+        col_id_ksiedza.setCellValueFactory(new PropertyValueFactory<>("id_ksiedza"));
+    }
 
-
+    private void tableViewPopulate()
+    {
+        try {
+            table_view.setItems(pobierzDane());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     @FXML
     private void switchToPrimary() throws IOException {
@@ -76,11 +103,8 @@ public class KsiegaOsob implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        tableViewSetup();
+        tableViewPopulate();
 
-        try {
-            System.out.println(pobierzDane()[0].imie);;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
--- client/src/main/java/edu/pwste/Osoba.java
+++ client/src/main/java/edu/pwste/Osoba.java
@@ -3,6 +3,54 @@ package edu.pwste;
 import java.util.Date;
 
 public class Osoba {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public Date getData_urodzenia() {
+        return data_urodzenia;
+    }
+
+    public void setData_urodzenia(Date data_urodzenia) {
+        this.data_urodzenia = data_urodzenia;
+    }
+
+    public String getMiejscowosc() {
+        return miejscowosc;
+    }
+
+    public void setMiejscowosc(String miejscowosc) {
+        this.miejscowosc = miejscowosc;
+    }
+
+    public int getId_ksiedza() {
+        return id_ksiedza;
+    }
+
+    public void setId_ksiedza(int id_ksiedza) {
+        this.id_ksiedza = id_ksiedza;
+    }
+
     public int id;
     public String imie;
     public String nazwisko;
