30c70d0c79cf93ca7f380a60adae49d7af7fe731
stealthoust <stealthoust@gmail.com>
2022-04-06 22:22:25 +0200

    formularze, navbary version 1.0

--- client/src/main/java/edu/pwste/Kalendarz.java
+++ client/src/main/java/edu/pwste/Kalendarz.java
@@ -1,20 +1,38 @@
 package edu.pwste;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.MenuButton;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
 
-public class Kalendarz {
+public class Kalendarz implements Initializable {
     public MenuButton mszaBtn;
+    public MenuButton formularzBtn;
+    public DatePicker datePicker;
+
+    LocalDate minDate = LocalDate.of(2010, 4, 16);
+    LocalDate maxDate = LocalDate.now();
+
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
     @FXML
-    private void switchToFormularz() throws IOException {
-        App.setRoot("formularz");
+    private void switchToFormularzUser() throws IOException {
+        formularzBtn.hide();
+        App.setRoot("formUser");
+    }
+    @FXML
+    private void switchToFormularzEvent() throws IOException {
+        formularzBtn.hide();
+        App.setRoot("formEvent");
     }
     @FXML
     private void switchToKalendarz() throws IOException {
@@ -23,6 +41,11 @@ public class Kalendarz {
     @FXML
     private void switchToKsiegaOsob() throws IOException {
         App.setRoot("ksiega_osob");
+    }
+    @FXML
+    private void switchToMszaEvent() throws IOException {
+        mszaBtn.hide();
+        App.setRoot("MszaEvent");
     }
     @FXML
     private void switchToMszaDodaj() throws IOException {
@@ -34,4 +57,15 @@ public class Kalendarz {
         mszaBtn.hide();
         App.setRoot("msza_info");
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        datePicker.setDayCellFactory(d ->
+                new DateCell() {
+                    @Override public void updateItem(LocalDate item, boolean empty) {
+                        super.updateItem(item, empty);
+                        setDisable(item.isAfter(maxDate) || item.isBefore(minDate));
+                    }});
+    }
+
 }
