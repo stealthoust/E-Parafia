360d8d7320ef6aed4e09b7cdfe7993a7417539e4
stealthoust <stealthoust@gmail.com>
2022-04-24 18:49:40 +0200

    dodaj_msze-inputs numbers only

--- client/src/main/java/edu/pwste/FormMsza.java
+++ client/src/main/java/edu/pwste/FormMsza.java
@@ -1,12 +1,23 @@
 package edu.pwste;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class FormMsza {
+public class FormMsza implements Initializable {
     public MenuButton mszaBtn,formularzBtn;
+    public TextField cmmTxt,cmrTxt,cmtgTxt,cmtmTxt,cmngTxt,cmnmTxt,mTxt,hTxt;
+    public Button btnDodaj,btnDodajcm;
+    public DatePicker kalendarz;
 
     @FXML
     private void switchToPrimary() throws IOException {
@@ -49,5 +60,94 @@ public class FormMsza {
     private void switchToMszaInfo() throws IOException {
         mszaBtn.hide();
         App.setRoot("msza_info");
+    }
+
+    private void inputSetup()
+    {
+        cmmTxt.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    cmmTxt.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+
+        cmrTxt.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    cmrTxt.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+
+        cmtgTxt.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    cmtgTxt.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+
+        cmtmTxt.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    cmtmTxt.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+
+        cmngTxt.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    cmngTxt.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+
+        cmnmTxt.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    cmnmTxt.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+
+        mTxt.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    mTxt.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+
+        hTxt.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    hTxt.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        inputSetup();
     }
 }
