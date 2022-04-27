0c212953ca278d95b44ed6cbabdf9dc29eabc9f3
stealthoust <stealthoust@gmail.com>
2022-04-27 13:45:34 +0200

    server and client sides, updates

--- server/routes/wydarzenia.js
+++ server/routes/wydarzenia.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const wydarzenia = require('../services/wydarzenia');
+const kalendarz = require("../services/kalendarz");
 
 router.post('/add', async function(req, res, next) {
     try {
@@ -10,6 +11,21 @@ router.post('/add', async function(req, res, next) {
         next(err);
     }
 });
+router.post('/addmw', async function(req, res, next) {
+    try {
+        res.json(await wydarzenia.addWydarzenieMsza(req.body.msza,req.body.wydarzenie));
+    } catch (err) {
+        console.error(`Error while getting programming languages `, err.message);
+        next(err);
+    }
+});
 
-
+router.get('/', async function(req, res, next) {
+    try {
+        res.json(await wydarzenia.getWydarzenia());
+    } catch (err) {
+        console.error(`Error while getting programming languages `, err.message);
+        next(err);
+    }
+});
 module.exports = router;
